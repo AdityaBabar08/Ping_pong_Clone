b261a4e782534f9817cf9825b4929dc6e72b5352
Aditya Babar <babaraditya02@gmail.com>
2025-03-31 16:26:08 +0530

    Added a paddles, ball and ball collision with screen border

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,60 @@
 #include "imguiThemes.h"
 #pragma endregion
 
+const int WINDOWWIDTH = 1280;
+const int WINDOWHEIGHT = 720;
+
+
+struct Ball
+{
+	Vector2 ballPos;
+	int speed_x, speed_y;
+	int radius;
+
+	void Draw();
+	void Update(float deltaTime);
+};
+
+
+void Ball::Draw()
+{
+	DrawCircle(ballPos.x, ballPos.y, radius, WHITE);
+}
+
+void Ball::Update(float deltaTIme)
+{
+	ballPos.x += speed_x * deltaTIme;
+	ballPos.y += speed_y * deltaTIme;
+
+
+	//border collisions
+	if ((ballPos.y + radius)>= WINDOWHEIGHT || (ballPos.y - radius) <= 0)
+	{
+		speed_y *= -1;
+	}
+	if ((ballPos.x + radius) >= WINDOWWIDTH || (ballPos.x - radius) <= 0)
+	{
+		speed_x *= -1;
+	}
+
+}
+
 
 
 int main(void)
 {
 
-	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-	InitWindow(800, 450, "raylib [core] example - basic window");
+	//SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+	InitWindow(WINDOWWIDTH, WINDOWHEIGHT, "raylib [core] example - basic window");
+
+	Ball ball{};
+	ball.ballPos = {
+		WINDOWWIDTH / 2.f,
+		WINDOWHEIGHT / 2.f
+	};
+	ball.speed_x = 500;
+	ball.speed_y = 500;
+	ball.radius = 15;
 
 #pragma region imgui
 	rlImGuiSetup(true);
@@ -43,46 +90,55 @@ int main(void)
 #pragma endregion
 
 
-
+	SetTargetFPS(60);
 	while (!WindowShouldClose())
 	{
-		BeginDrawing();
-		ClearBackground(RAYWHITE);
+		float deltaTime = GetFrameTime();
+
+#pragma region Update
+
+		ball.Update(deltaTime);
+
+#pragma endregion
 
 
-	#pragma region imgui
-		rlImGuiBegin();
+#pragma region Drawing
 
-		ImGui::PushStyleColor(ImGuiCol_WindowBg, {});
-		ImGui::PushStyleColor(ImGuiCol_DockingEmptyBg, {});
-		ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
-		ImGui::PopStyleColor(2);
-	#pragma endregion
-
-
-		ImGui::Begin("Test");
-
-		ImGui::Text("Hello");
-		ImGui::Button("Button");
-		ImGui::Button("Button2");
-
-		ImGui::End();
-
-
-		DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-
-
-	#pragma region imgui
-		rlImGuiEnd();
-
-		if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
 		{
-			ImGui::UpdatePlatformWindows();
-			ImGui::RenderPlatformWindowsDefault();
-		}
-	#pragma endregion
+			BeginDrawing();
+			ClearBackground(BLACK);
 
-		EndDrawing();
+
+#pragma region imgui
+			rlImGuiBegin();
+
+			ImGui::PushStyleColor(ImGuiCol_WindowBg, {});
+			ImGui::PushStyleColor(ImGuiCol_DockingEmptyBg, {});
+			ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
+			ImGui::PopStyleColor(2);
+#pragma endregion
+			DrawLine(WINDOWWIDTH / 2.f, 0, WINDOWWIDTH / 2.f, WINDOWHEIGHT, GREEN);
+			DrawRectangle(10, WINDOWHEIGHT/2.f - 50, 15, 100, WHITE);
+			DrawRectangle(WINDOWWIDTH - 25, WINDOWHEIGHT/2.f - 50, 15, 100, WHITE);
+			ball.Draw();
+
+
+
+#pragma region imgui
+			rlImGuiEnd();
+
+			if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+			{
+				ImGui::UpdatePlatformWindows();
+				ImGui::RenderPlatformWindowsDefault();
+			}
+#pragma endregion
+
+			EndDrawing();
+		}
+
+#pragma endregion
+
 	}
 
 
