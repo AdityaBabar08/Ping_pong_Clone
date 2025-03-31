ca789d429b6900aa17823ed831bca6b892abb572
Aditya Babar <babaraditya02@gmail.com>
2025-03-31 19:50:07 +0530

    Completed the 80% of pong game

--- src/main.cpp
+++ src/main.cpp
@@ -45,6 +45,57 @@ void Ball::Update(float deltaTIme)
 
 }
 
+struct Paddle
+{
+	Vector2 paddlePos;
+	int paddleWidth;
+	int paddleHeight;
+	int paddleSpeed;
+
+	void Draw();
+	void Update(float deltaTime);
+	void CpuUpdate(float deltaTime, Ball& ball);
+};
+
+void Paddle::Draw()
+{
+	DrawRectangle(paddlePos.x, paddlePos.y, paddleWidth, paddleHeight, WHITE);
+}
+
+void Paddle::Update(float deltaTime)
+{
+	
+	if (IsKeyDown(KEY_UP) && paddlePos.y >= 10)
+	{
+		paddlePos.y = paddlePos.y - paddleSpeed * deltaTime;
+	}
+	if (IsKeyDown(KEY_DOWN) && paddlePos.y <= (WINDOWHEIGHT - paddleHeight) - 10)
+	{
+		paddlePos.y = paddlePos.y + paddleSpeed * deltaTime;
+	}
+}
+
+void Paddle::CpuUpdate(float deltaTime, Ball& ball)
+{
+	if (paddlePos.y + paddleHeight / 2 > ball.ballPos.y)
+	{
+		paddlePos.y = paddlePos.y - paddleSpeed * deltaTime;
+	}
+	if (paddlePos.y + paddleHeight / 2 < ball.ballPos.y)
+	{
+		paddlePos.y = paddlePos.y + paddleSpeed * deltaTime;
+	}
+
+	if (paddlePos.y <= 0)
+	{
+		paddlePos.y = 0;
+	}
+	if (paddlePos.y + paddleHeight >= WINDOWHEIGHT)
+	{
+		paddlePos.y = WINDOWHEIGHT - paddleHeight;
+	}
+
+}
 
 
 int main(void)
@@ -52,6 +103,10 @@ int main(void)
 
 	//SetConfigFlags(FLAG_WINDOW_RESIZABLE);
 	InitWindow(WINDOWWIDTH, WINDOWHEIGHT, "raylib [core] example - basic window");
+	SetTargetFPS(60);
+
+	int playerScore = 0;
+	int cpuScore = 0;
 
 	Ball ball{};
 	ball.ballPos = {
@@ -62,6 +117,23 @@ int main(void)
 	ball.speed_y = 500;
 	ball.radius = 15;
 
+	Paddle player{};
+	player.paddlePos = {
+		10,
+		WINDOWHEIGHT / 2.f - 50
+	};
+	player.paddleWidth = 15;
+	player.paddleHeight = 100;
+	player.paddleSpeed = 700;
+
+	Paddle cpu{};
+	cpu.paddlePos = {
+		WINDOWWIDTH - 25,
+		WINDOWHEIGHT / 2.f - 50
+	};
+	cpu.paddleWidth = 15;
+	cpu.paddleHeight = 100;
+	cpu.paddleSpeed = 700;
 #pragma region imgui
 	rlImGuiSetup(true);
 
@@ -90,7 +162,7 @@ int main(void)
 #pragma endregion
 
 
-	SetTargetFPS(60);
+	
 	while (!WindowShouldClose())
 	{
 		float deltaTime = GetFrameTime();
@@ -98,6 +170,55 @@ int main(void)
 #pragma region Update
 
 		ball.Update(deltaTime);
+		player.Update(deltaTime);
+		cpu.CpuUpdate(deltaTime, ball);
+
+
+
+		bool playerCollided = CheckCollisionCircleRec(
+			ball.ballPos,
+			ball.radius,
+			{
+				player.paddlePos.x,
+				player.paddlePos.y,
+				(float)player.paddleWidth,
+				(float)player.paddleHeight
+			}
+		);
+
+
+		if (playerCollided)
+		{
+			ball.speed_x *= -1;
+		}
+
+
+		bool cpuCollided = CheckCollisionCircleRec(
+			ball.ballPos,
+			ball.radius,
+			{
+				cpu.paddlePos.x,
+				cpu.paddlePos.y,
+				(float)cpu.paddleWidth,
+				(float)cpu.paddleHeight
+			}
+		);
+
+
+		if (cpuCollided)
+		{
+			ball.speed_x *= -1;
+		}
+
+		if ((ball.ballPos.x + ball.radius) >= WINDOWWIDTH)
+		{
+			playerScore++;
+		}
+			
+		if ((ball.ballPos.x - ball.radius) <= 0)
+		{
+			cpuScore++;
+		}
 
 #pragma endregion
 
@@ -117,11 +238,17 @@ int main(void)
 			ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
 			ImGui::PopStyleColor(2);
 #pragma endregion
-			DrawLine(WINDOWWIDTH / 2.f, 0, WINDOWWIDTH / 2.f, WINDOWHEIGHT, GREEN);
-			DrawRectangle(10, WINDOWHEIGHT/2.f - 50, 15, 100, WHITE);
-			DrawRectangle(WINDOWWIDTH - 25, WINDOWHEIGHT/2.f - 50, 15, 100, WHITE);
-			ball.Draw();
+			
 
+			DrawLine(WINDOWWIDTH / 2.f, 0, WINDOWWIDTH / 2.f, WINDOWHEIGHT, GREEN);
+			//DrawRectangle(10, WINDOWHEIGHT/2.f - 50, 15, 100, WHITE);
+			player.Draw();
+			//DrawRectangle(WINDOWWIDTH - 25, WINDOWHEIGHT/2.f - 50, 15, 100, WHITE);
+			cpu.Draw();
+			ball.Draw();
+			DrawText(TextFormat("%i", playerScore), WINDOWWIDTH / 4.f, 20, 100, WHITE);
+			DrawText(TextFormat("%i", cpuScore), (WINDOWWIDTH - (WINDOWWIDTH / 4.f)), 20, 100, WHITE);
+			
 
 
 #pragma region imgui
