c7c353a81a8c2a8afde120952e8ff3d7a492f593
Aditya Babar <babaraditya02@gmail.com>
2025-04-01 13:45:11 +0530

    Polished the game

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,10 @@
 const int WINDOWWIDTH = 1280;
 const int WINDOWHEIGHT = 720;
 
-
+Color Light_green = Color{ 129, 204, 184, 255 };
+Color Green = Color{ 38, 185, 154, 255 };
+Color Dark_Green = Color{ 20, 160, 133, 255 };
+Color Yellow = Color{ 243, 213, 91, 255 };
 struct Ball
 {
 	Vector2 ballPos;
@@ -24,7 +27,7 @@ struct Ball
 
 void Ball::Draw()
 {
-	DrawCircle(ballPos.x, ballPos.y, radius, WHITE);
+	DrawCircle(ballPos.x, ballPos.y, radius, Yellow);
 }
 
 void Ball::Update(float deltaTIme)
@@ -34,7 +37,7 @@ void Ball::Update(float deltaTIme)
 
 
 	//border collisions
-	if ((ballPos.y + radius)>= WINDOWHEIGHT || (ballPos.y - radius) <= 0)
+	if ((ballPos.y + radius) >= WINDOWHEIGHT || (ballPos.y - radius) <= 0)
 	{
 		speed_y *= -1;
 	}
@@ -64,7 +67,7 @@ void Paddle::Draw()
 
 void Paddle::Update(float deltaTime)
 {
-	
+
 	if (IsKeyDown(KEY_UP) && paddlePos.y >= 10)
 	{
 		paddlePos.y = paddlePos.y - paddleSpeed * deltaTime;
@@ -97,6 +100,8 @@ void Paddle::CpuUpdate(float deltaTime, Ball& ball)
 
 }
 
+enum GameState { MENU, PLAYING, GAME_OVER };
+GameState currentState = MENU;
 
 int main(void)
 {
@@ -146,12 +151,12 @@ int main(void)
 	//imguiThemes::embraceTheDarkness();
 
 
-	ImGuiIO &io = ImGui::GetIO(); (void)io;
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
 	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
 	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
 	io.FontGlobalScale = 2;
 
-	ImGuiStyle &style = ImGui::GetStyle();
+	ImGuiStyle& style = ImGui::GetStyle();
 	if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
 	{
 		//style.WindowRounding = 0.0f;
@@ -162,85 +167,112 @@ int main(void)
 #pragma endregion
 
 
-	
+
 	while (!WindowShouldClose())
 	{
 		float deltaTime = GetFrameTime();
-
-#pragma region Update
-
-		ball.Update(deltaTime);
-		player.Update(deltaTime);
-		cpu.CpuUpdate(deltaTime, ball);
-
-
-
-		bool playerCollided = CheckCollisionCircleRec(
-			ball.ballPos,
-			ball.radius,
-			{
-				player.paddlePos.x,
-				player.paddlePos.y,
-				(float)player.paddleWidth,
-				(float)player.paddleHeight
-			}
-		);
-
-
-		if (playerCollided)
-		{
-			ball.speed_x *= -1;
-		}
-
-
-		bool cpuCollided = CheckCollisionCircleRec(
-			ball.ballPos,
-			ball.radius,
-			{
-				cpu.paddlePos.x,
-				cpu.paddlePos.y,
-				(float)cpu.paddleWidth,
-				(float)cpu.paddleHeight
-			}
-		);
-
-
-		if (cpuCollided)
-		{
-			ball.speed_x *= -1;
-		}
-
-		if ((ball.ballPos.x + ball.radius) >= WINDOWWIDTH)
-		{
-			playerScore++;
-		}
-			
-		if ((ball.ballPos.x - ball.radius) <= 0)
-		{
-			cpuScore++;
-		}
-
-#pragma endregion
-
-
-#pragma region Drawing
-
-		{
-			BeginDrawing();
-			ClearBackground(BLACK);
+		BeginDrawing();
+		ClearBackground(Dark_Green);
 
 
 #pragma region imgui
-			rlImGuiBegin();
+		rlImGuiBegin();
 
-			ImGui::PushStyleColor(ImGuiCol_WindowBg, {});
-			ImGui::PushStyleColor(ImGuiCol_DockingEmptyBg, {});
-			ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
-			ImGui::PopStyleColor(2);
+		ImGui::PushStyleColor(ImGuiCol_WindowBg, {});
+		ImGui::PushStyleColor(ImGuiCol_DockingEmptyBg, {});
+		ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
+		ImGui::PopStyleColor(2);
 #pragma endregion
-			
 
-			DrawLine(WINDOWWIDTH / 2.f, 0, WINDOWWIDTH / 2.f, WINDOWHEIGHT, GREEN);
+
+
+
+		switch (currentState)
+		{
+		case MENU:
+			ImGui::Begin("Pong Menu");
+			if (ImGui::Button("Start Game"))
+			{
+				currentState = PLAYING;
+				playerScore = 0;
+				cpuScore = 0;
+				ball.ballPos = { WINDOWWIDTH / 2.f, WINDOWHEIGHT / 2.f };
+				ball.speed_x = 500;
+				ball.speed_y = 500;
+			}
+			if (ImGui::Button("Exit"))
+			{
+				CloseWindow();
+				return 0;
+			}
+			ImGui::End();
+			break;
+		case PLAYING:
+#pragma region Update
+
+		{
+			ball.Update(deltaTime);
+			player.Update(deltaTime);
+			cpu.CpuUpdate(deltaTime, ball);
+
+
+
+			bool playerCollided = CheckCollisionCircleRec(
+				ball.ballPos,
+				ball.radius,
+				{
+					player.paddlePos.x,
+					player.paddlePos.y,
+					(float)player.paddleWidth,
+					(float)player.paddleHeight
+				}
+			);
+
+
+			if (playerCollided)
+			{
+				float offset = (ball.ballPos.y - (player.paddlePos.y + player.paddleHeight / 2.0f)) / (player.paddleHeight / 2.0f);
+				float angle = offset * (PI / 4); // Max angle of 45 degrees
+				ball.speed_x = cos(angle) * 500; // Positive x-speed (to the right)
+				ball.speed_y = sin(angle) * 500;
+			}
+
+
+			bool cpuCollided = CheckCollisionCircleRec(
+				ball.ballPos,
+				ball.radius,
+				{
+					cpu.paddlePos.x,
+					cpu.paddlePos.y,
+					(float)cpu.paddleWidth,
+					(float)cpu.paddleHeight
+				}
+			);
+
+
+			if (cpuCollided)
+			{
+				float offset = (ball.ballPos.y - (cpu.paddlePos.y + cpu.paddleHeight / 2.0f)) / (cpu.paddleHeight / 2.0f);
+				float angle = offset * (PI / 4);
+				ball.speed_x = -cos(angle) * 500; // Negative x-speed (to the left)
+				ball.speed_y = sin(angle) * 500;
+			}
+
+			if ((ball.ballPos.x + ball.radius) >= WINDOWWIDTH)
+			{
+				playerScore++;
+			}
+
+			if ((ball.ballPos.x - ball.radius) <= 0)
+			{
+				cpuScore++;
+			}
+		}
+
+#pragma endregion
+			DrawRectangle(0, 0, WINDOWWIDTH / 2.f, WINDOWHEIGHT, Green);
+			DrawCircle(WINDOWWIDTH / 2.f, WINDOWHEIGHT / 2.f, 100, Light_green);
+			DrawLine(WINDOWWIDTH / 2.f, 0, WINDOWWIDTH / 2.f, WINDOWHEIGHT, WHITE);
 			//DrawRectangle(10, WINDOWHEIGHT/2.f - 50, 15, 100, WHITE);
 			player.Draw();
 			//DrawRectangle(WINDOWWIDTH - 25, WINDOWHEIGHT/2.f - 50, 15, 100, WHITE);
@@ -248,21 +280,42 @@ int main(void)
 			ball.Draw();
 			DrawText(TextFormat("%i", playerScore), WINDOWWIDTH / 4.f, 20, 100, WHITE);
 			DrawText(TextFormat("%i", cpuScore), (WINDOWWIDTH - (WINDOWWIDTH / 4.f)), 20, 100, WHITE);
-			
+
+			break;
+		case GAME_OVER:
+			ImGui::Begin("Game Over");
+			if (playerScore >= 10)
+				ImGui::Text("Player Wins!");
+			else
+				ImGui::Text("CPU Wins!");
+			if (ImGui::Button("Restart"))
+			{
+				currentState = PLAYING;
+				playerScore = 0;
+				cpuScore = 0;
+				ball.ballPos = { WINDOWWIDTH / 2.f, WINDOWHEIGHT / 2.f };
+				ball.speed_x = 500;
+				ball.speed_y = 500;
+			}
+			ImGui::End();
+			break;
+		}
+
+
 
 
 #pragma region imgui
-			rlImGuiEnd();
+		rlImGuiEnd();
 
-			if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
-			{
-				ImGui::UpdatePlatformWindows();
-				ImGui::RenderPlatformWindowsDefault();
-			}
+		if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+		{
+			ImGui::UpdatePlatformWindows();
+			ImGui::RenderPlatformWindowsDefault();
+		}
 #pragma endregion
 
-			EndDrawing();
-		}
+		EndDrawing();
+
 
 #pragma endregion
 
